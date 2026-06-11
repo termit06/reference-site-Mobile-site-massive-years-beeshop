90cd2eeabbdee77d616dff933c17cb0176157745
Sk1L3D <146962543+termit06@users.noreply.github.com>
2026-06-11 10:59:31 +0300

    Delete src/components/Calculator.tsx directory

--- src/components/Calculator.tsx/CalculatorField.tsx
+++
@@ -1,8 +0,0 @@
-import { TextField } from "@mui/material";
-interface calculatorFieldProps {
-    inputString: string
-}
-const CalculatorField = (props: calculatorFieldProps) => {
-    return <TextField value={props.inputString}/>
-}
-export default CalculatorField;
